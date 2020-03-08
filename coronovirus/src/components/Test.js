8cf2dba35c405e1f6b86742c85781fb5ca60aba2
Bridget <bridgetoconnor808@gmail.com>
2020-03-07 17:49:31 -0800

    added map to test page

--- coronovirus/src/components/Test.js
+++ coronovirus/src/components/Test.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Typography from '@material-ui/core/Typography';
+import Map from './Map';
 
 function Test() {
   return (
     <div>
       <Typography> Test Page </Typography>
+      <Map/>
     </div>
   );
 }
