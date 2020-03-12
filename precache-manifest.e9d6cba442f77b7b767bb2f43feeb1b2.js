f668bb3b06dab55e15f218d06104d1049df87faf
Bridget <bridgetoconnor808@gmail.com>
2020-03-11 22:42:40 -0700

    Updates

---
+++ precache-manifest.e9d6cba442f77b7b767bb2f43feeb1b2.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72a12892f35d1bb6c7a74393b8631cee",
+    "url": "/CoroNo/index.html"
+  },
+  {
+    "revision": "51f10fdda59b459ccd66",
+    "url": "/CoroNo/static/css/main.76225922.chunk.css"
+  },
+  {
+    "revision": "4c9aaf731d4d0f72bb13",
+    "url": "/CoroNo/static/js/2.6dfb6e5f.chunk.js"
+  },
+  {
+    "revision": "a5288a4ed4e4beba8413aa5c9ae969ea",
+    "url": "/CoroNo/static/js/2.6dfb6e5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51f10fdda59b459ccd66",
+    "url": "/CoroNo/static/js/main.c807f686.chunk.js"
+  },
+  {
+    "revision": "eccf001fb13e4df23af1",
+    "url": "/CoroNo/static/js/runtime-main.b09e8fe0.js"
+  },
+  {
+    "revision": "2f228e5ecbc7e9296984476ceac7e3aa",
+    "url": "/CoroNo/static/media/cdc.2f228e5e.png"
+  },
+  {
+    "revision": "41520073ae649dabe9298163012e37e9",
+    "url": "/CoroNo/static/media/coughManners.41520073.png"
+  },
+  {
+    "revision": "12f5dd18066a87851ef75c64a16640da",
+    "url": "/CoroNo/static/media/directRelief.12f5dd18.png"
+  },
+  {
+    "revision": "4b7df9e4e559c391a20e3ea759bdf2c5",
+    "url": "/CoroNo/static/media/eyesNoseMouth.4b7df9e4.png"
+  },
+  {
+    "revision": "f802b2c6099073962a5426f7e4784467",
+    "url": "/CoroNo/static/media/globalGiving.f802b2c6.png"
+  },
+  {
+    "revision": "46e82d4cf945c6e71efe846da619572e",
+    "url": "/CoroNo/static/media/h2h.46e82d4c.png"
+  },
+  {
+    "revision": "86785035473e6d786e90270f08444b53",
+    "url": "/CoroNo/static/media/masks.86785035.png"
+  },
+  {
+    "revision": "74248bf48f8ffc8689b5328ce728179e",
+    "url": "/CoroNo/static/media/onlyAsians.74248bf4.png"
+  },
+  {
+    "revision": "957fe607ce41652fdf07b8f223aa4707",
+    "url": "/CoroNo/static/media/onlyOldPeople.957fe607.png"
+  },
+  {
+    "revision": "5d558be01c5cfe0920381fddb25c8263",
+    "url": "/CoroNo/static/media/sameAsFlu.5d558be0.png"
+  },
+  {
+    "revision": "aa83c2df2184d0a31296da3715c00cc8",
+    "url": "/CoroNo/static/media/socialDistance.aa83c2df.png"
+  },
+  {
+    "revision": "87d938202e125fe556c917fc4ecaa803",
+    "url": "/CoroNo/static/media/spreadContact.87d93820.png"
+  },
+  {
+    "revision": "6b91c63690253fdfc940721d4c5aab02",
+    "url": "/CoroNo/static/media/spreadMap.6b91c636.png"
+  },
+  {
+    "revision": "a88b9440fad54a21e090e59f15143fd4",
+    "url": "/CoroNo/static/media/washHands.a88b9440.png"
+  }
+]);
